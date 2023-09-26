ae73c33dfca7a152ee731a32cff645466f909c77
voikin <voikin.a@edu.rea.ru>
2023-09-27 01:06:26 +0300

    добавил класс доступа к бд и реализовал чтение и запись данных из json

--- game-2048/Program.cs
+++ game-2048/Program.cs
@@ -1,1 +1,6 @@
-﻿
+﻿using game_2048;
+using game_2048.DataLayer;
+
+PlayerRecordDTO firstOne = new PlayerRecordDTO("Arseni");
+List<PlayerRecordDTO> records = new List<PlayerRecordDTO>() { firstOne };
+DataAccess.SaveRecords(records);
