e638730e700e9316507bf82bc71ecf1a26b044e8
Nikita Panferov <nikpanfer@gmail.com>
2023-10-27 19:35:42 +0300

    Change PlayerRecordDTO to record

--- game-2048/DataLayer/dtos/PlayerRecordDTO.cs
+++ game-2048/DataLayer/dtos/PlayerRecordDTO.cs
@@ -1,13 +1,14 @@
 namespace game_2048.DataLayer.dtos;
 
-public class PlayerRecordDTO
+public record PlayerRecordDTO
 {
-    public string Name { get; }
+    public string Name { get; init; }
     public int HighScore { get; set; }
-
+    
     public PlayerRecordDTO(string name, int highScore = 0)
     {
         Name = name;
         HighScore = highScore;
     }
+
 }
